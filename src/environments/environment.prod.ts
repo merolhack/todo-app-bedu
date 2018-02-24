833ca324247609696ce33b142c48d17f80384247
Lenin Meza <merolhack@gmail.com>
2018-02-24 05:46:48 -0600

    :sparkles: Firebase configuration files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC0xfd_QbFHT3IV8C5wrlIzzTLJ6cHKFEE",
+    authDomain: "todo-app-bedu.firebaseapp.com",
+    databaseURL: "https://todo-app-bedu.firebaseio.com",
+    projectId: "todo-app-bedu",
+    storageBucket: "todo-app-bedu.appspot.com",
+    messagingSenderId: "394568616960"
+  }
 };
